f3f5406d90dc105661ee22b8e7758f49eef70831
ForwarD_NerN <mlun42941@gmail.com>
2024-03-15 22:42:57 +0500

    Removed LOGGER. System.out.println should be used instead

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -6,10 +6,9 @@ import java.util.logging.Logger;
 
 public class ExampleMod implements ModInitializer {
     public static final String MOD_ID = "examplemod";
-    public static final Logger LOGGER = Logger.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Hello non Cosmic world!");
+        System.out.println("Hello non Cosmic world!");
     }
 }
